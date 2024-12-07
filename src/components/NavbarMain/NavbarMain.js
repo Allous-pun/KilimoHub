0339610674193399c6729f022a1ac7dcb9af0415
pundit <pundit@pundit.pundit.com>
2024-12-07 13:46:24 +0300

    updated nav

--- src/components/NavbarMain/NavbarMain.js
+++ src/components/NavbarMain/NavbarMain.js
@@ -3,25 +3,37 @@ import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import styles from "./NavbarMain.module.css";
 import { NavLink } from "react-router-dom";
 import { Link } from 'react-router-dom';
-const NavbarMain = () => {
 
-  const [show, setShow] = useState(false);
+const NavbarMain = () => {
+  const [showServices, setShowServices] = useState(false);
+  const [showRegistration, setShowRegistration] = useState(false);
   const [activeNav, setActiveNav] = useState([ true, false, false, false ]);
   const [expand, setExpand] = useState(false);
 
-  const closeNav = ()=>{
+  const closeNav = () => {
     setExpand(false);
   }
 
-  const showDropdown = (e) => {
-    setShow(!show);
+  const showServicesDropdown = () => {
+    setShowServices(true);
+    setShowRegistration(false); // Close the Registration dropdown when opening Services
   };
-  const hideDropdown = (e) => {
-    setShow(false);
+
+  const hideServicesDropdown = () => {
+    setShowServices(false);
+  };
+
+  const showRegistrationDropdown = () => {
+    setShowRegistration(true);
+    setShowServices(false); // Close the Services dropdown when opening Registration
+  };
+
+  const hideRegistrationDropdown = () => {
+    setShowRegistration(false);
   };
 
   useEffect(() => {
-    if (sessionStorage.getItem("NavbarMain") != null) {
+    if (sessionStorage.getItem("NavbarMain") !== null) {
       let temp = JSON.parse(sessionStorage.getItem("NavbarMain"));
       setActiveNav([...temp]);
     }
@@ -29,14 +41,11 @@ const NavbarMain = () => {
 
   const handleActiveNav = (i) => {
     let temp = activeNav;
-    temp = temp.map((x) => (x = false));
+    temp = temp.map(() => false);
     temp[i] = true;
     setActiveNav([...temp]);
     sessionStorage.setItem("NavbarMain", JSON.stringify(temp));
-
   };
-
-
 
   return (
     <>
@@ -48,22 +57,22 @@ const NavbarMain = () => {
         variant="light"
         expand="lg"
         sticky="top"
-        onToggle={()=>{setExpand(prevState => !prevState)}}
+        onToggle={() => { setExpand(prevState => !prevState) }}
         expanded={expand}
       >
         <Container>
           <Navbar.Brand href="/" className={styles.logo}>
-            Miritus
+            <img src="/logo.png" alt="FarmUnity Logo" className={styles.logoImage} />
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav style={{marginLeft:'auto'}} >
+            <Nav style={{ marginLeft: 'auto' }}>
               <NavLink
                 to="/"
                 className={`${styles.nav_text} nav-link ${activeNav[0] ? styles.active : ""}`}
-                style={{ marginTop: "8px"}}
-                onClick={() => {handleActiveNav(0); closeNav()}}
+                style={{ marginTop: "8px" }}
+                onClick={() => { handleActiveNav(0); closeNav() }}
               >
                 Home
               </NavLink>
@@ -72,39 +81,56 @@ const NavbarMain = () => {
                 to="/aboutUs"
                 className={`${styles.nav_text} nav-link ${activeNav[1] ? styles.active : ""}`}
                 style={{ marginTop: "8px" }}
-                onClick={() => {handleActiveNav(1); closeNav()}}
+                onClick={() => { handleActiveNav(1); closeNav() }}
               >
                 About us
               </NavLink>
 
               <NavDropdown
-                show={show}
-                onMouseEnter={showDropdown}
-                onMouseLeave={hideDropdown}
+                show={showServices}
+                onMouseEnter={showServicesDropdown}
+                onMouseLeave={hideServicesDropdown}
                 className={`nav-link ${styles.drop}`}
                 title={
-                  <Link to="/services" style={{textDecoration:'none'}} className={styles.dropicon} onClick={() => {handleActiveNav(2); closeNav()}}>
+                  <Link to="/services" style={{ textDecoration: 'none' }} className={styles.dropicon} onClick={() => { handleActiveNav(2); closeNav() }}>
                     <span className={`${styles.nav_text} my-auto ${activeNav[2] ? styles.active : ""}`}>
                       Services
                     </span>
                   </Link>
                 }
-                id="basic-nav-dropdown"
+                id="services-nav-dropdown"
               >
-                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/sMediaService" onClick={() => {handleActiveNav(2); closeNav()}} className={styles.dropdownText}>Social Media Marketing</NavLink></NavDropdown.Item>
-                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => {handleActiveNav(2); closeNav()}} className={styles.dropdownText}>Quality Assurance</NavLink></NavDropdown.Item>
-                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => {handleActiveNav(2); closeNav()}} className={styles.dropdownText}>Cloud and IoT Based Solutions</NavLink></NavDropdown.Item>
-                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => {handleActiveNav(2); closeNav()}} className={styles.dropdownText}>IT Consultancy</NavLink></NavDropdown.Item>
-                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => {handleActiveNav(2); closeNav()}} className={styles.dropdownText}>AI Solutions</NavLink></NavDropdown.Item>
-
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/sMediaService" onClick={() => { handleActiveNav(2); closeNav() }} className={styles.dropdownText}>Social Media Marketing</NavLink></NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => { handleActiveNav(2); closeNav() }} className={styles.dropdownText}>Quality Assurance</NavLink></NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => { handleActiveNav(2); closeNav() }} className={styles.dropdownText}>Cloud and IoT Based Solutions</NavLink></NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => { handleActiveNav(2); closeNav() }} className={styles.dropdownText}>IT Consultancy</NavLink></NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/hello" onClick={() => { handleActiveNav(2); closeNav() }} className={styles.dropdownText}>AI Solutions</NavLink></NavDropdown.Item>
               </NavDropdown>
 
-              
+              {/* Registration Dropdown */}
+              <NavDropdown
+                show={showRegistration}
+                onMouseEnter={showRegistrationDropdown}
+                onMouseLeave={hideRegistrationDropdown}
+                className={`nav-link ${styles.drop}`}
+                title={
+                  <Link to="/#" style={{ textDecoration: 'none' }} className={styles.dropicon} onClick={() => { handleActiveNav(3); closeNav() }}>
+                    <span className={`${styles.nav_text} my-auto ${activeNav[3] ? styles.active : ""}`}>
+                      Registration
+                    </span>
+                  </Link>
+                }
+                id="registration-nav-dropdown"
+              >
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/signUp" onClick={() => { handleActiveNav(3); closeNav() }} className={styles.dropdownText}>Sign Up</NavLink></NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}><NavLink to="/login" onClick={() => { handleActiveNav(3); closeNav() }} className={styles.dropdownText}>Login</NavLink></NavDropdown.Item>
+              </NavDropdown>
+
               <NavLink
                 to="/contactUs"
-                className={` ${styles.nav_text} nav-link ${activeNav[3] ? styles.active : ""}`}
+                className={`${styles.nav_text} nav-link ${activeNav[4] ? styles.active : ""}`}
                 style={{ marginTop: "8px" }}
-                onClick={() => {handleActiveNav(3); closeNav()}}
+                onClick={() => { handleActiveNav(4); closeNav() }}
               >
                 Contact Us
               </NavLink>
