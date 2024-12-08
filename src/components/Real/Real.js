95c5ff570b46b54609d905daf0d24f6e099e59c8
pundit <pundit@pundit.pundit.com>
2024-12-08 09:28:05 +0300

    updated home

--- src/components/Real/Real.js
+++ src/components/Real/Real.js
@@ -1,26 +1,31 @@
 import React from "react";
 import styles from './Real.module.css';
-import improveImg from '../../assets/home/improves.png';
+import farmUnityImg from '../../assets/home/improves.png';
 
-const Real = () =>{
-    return(
+const Real = () => {
+    return (
         <div className="mb-5 overflow-hidden">
             <div className="row g-0">
                 <div className={`col-md-6 ${styles.imgDiv}`}>
-                    <img data-aos='slide-right' data-aos-offset="150" src={improveImg} alt="" className={styles.img}/>
+                    <img data-aos='slide-right' data-aos-offset="150" src={farmUnityImg} alt="FarmUnity" className={styles.img} />
                 </div>
                 <div className="col-md-6">
                     <div className={styles.text}>
-                        <p data-aos='slide-right' data-aos-offset="150"  className={styles.heading}>Real</p>
-                        <p data-aos='slide-left' data-aos-offset="150" className={styles.subHeading}>Improves Customer Experiences</p>
-                        <p data-aos='zoom-in' data-aos-offset="150" className={styles.content}>Responsive web design supply chain crowdfunding agile development analytics technology ownership startup. Network effects social proof user experience prototype buzz product management deployment metrics sales infographic equity holy grail crowdfunding release. </p>
-                        <div data-aos='zoom-in' style={{textAlign:'left'}}>
-                            <button className={`btn custom_btn ${styles.btn}`}>LET&apos;S TALK</button>
+                        <p data-aos='slide-right' data-aos-offset="150" className={styles.heading}>FarmUnity</p>
+                        <p data-aos='slide-left' data-aos-offset="150" className={styles.subHeading}>Empowering Farmers, Building Communities</p>
+                        <p data-aos='zoom-in' data-aos-offset="150" className={styles.content}>
+                            FarmUnity transforms agriculture by connecting farmers to resources, markets, and financial services. 
+                            Our platform promotes collaborative farming, sustainable practices, and innovative technology 
+                            to help farmers maximize yields, reduce costs, and thrive in a competitive market.
+                        </p>
+                        <div data-aos='zoom-in' style={{ textAlign: 'left' }}>
+                            <button className={`btn custom_btn ${styles.btn}`}>JOIN THE COMMUNITY</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
 export default Real;
