49fee06fadf1b70d9a41c5f7d1ef95d8de180308
pundit <pundit@pundit.pundit.com>
2024-12-10 15:15:13 +0300

    updated aboutus

--- src/components/AboutUs/Top/Top.js
+++ src/components/AboutUs/Top/Top.js
@@ -6,28 +6,28 @@ import img2 from '../../../assets/aboutUs/img2.png';
 const Top = () => {
     return (
         <div className={`container overflow-hidden`}>
-            <p data-aos='slide-right' className={styles.heading}>About FarmUnity</p>
-            <p data-aos='slide-left' className={styles.subHeading}>Empowering Farmers, Revolutionizing Agriculture</p>
+            <p data-aos='slide-right' className={styles.heading}>About MazaoHub</p>
+            <p data-aos='slide-left' className={styles.subHeading}>Your Partner in Better Yield</p>
             <p data-aos='fade-up' data-aos-offset='80' className={styles.content}>
-                FarmUnity is a digital platform designed to empower small-scale farmers by addressing the critical challenges they face in agriculture. 
-                Acting as a one-stop solution, FarmUnity connects farmers with buyers, input suppliers, financial institutions, and agro-professionals, enabling them to operate efficiently and profitably.
+                MazaoHub is a digital platform designed to empower small-scale farmers by addressing the critical challenges they face in agriculture. 
+                Acting as a one-stop solution, MazaoHub connects farmers with buyers, input suppliers, and agro-professionals, enabling them to operate efficiently and profitably.
             </p>
+
 
             <div className={`row ${styles.contain} flex-column-reverse flex-md-row`}>
                 <div data-aos='slide-right' className={`col-md-6`}>
                     <p className={styles.about}>About Us</p>
                     <p className={styles.creativity}>We Foster Collaboration and Innovation</p>
                     <p className={styles.contn}>
-                        By leveraging technology, FarmUnity facilitates:
+                        MazaoHub is a digital platform designed to empower small-scale farmers by addressing the critical challenges they face in agriculture. The platform serves as a one-stop solution that connects farmers with buyers, input suppliers, financial institutions, and agro-professionals, enabling them to operate more efficiently and profitably.
                         <ul>
-                            <li><b>Collective Purchasing:</b> Farmers pool resources to reduce costs for inputs like seeds, fertilizers, and tools.</li>
-                            <li><b>Direct Market Access:</b> Farmers sell produce directly to wholesalers, retailers, and cooperatives at fair prices.</li>
-                            <li><b>Financial Inclusion:</b> Partnerships with institutions provide microloans, insurance, and savings plans tailored for farmers.</li>
-                            <li><b>Real-Time Data Insights:</b> Farmers access market trends, weather forecasts, and crop-specific advice for optimal productivity.</li>
-                            <li><b>Supportive Ecosystem:</b> Agro-professionals, extension officers, and training resources ensure farmers succeed.</li>
+                            <li><b>Collective Purchasing:</b> Farmers pool their resources to buy inputs like seeds, fertilizers, and tools in bulk, reducing costs significantly.</li>
+                            <li><b>Direct Market Access:</b> The platform eliminates exploitative intermediaries, allowing farmers to sell their produce directly to wholesalers, retailers, and cooperatives at fair prices.</li>
+                            <li><b>Real-Time Data Insights:</b> Farmers are equipped with data on market trends, weather forecasts, and crop-specific advice, enabling them to make informed decisions and optimize their productivity.</li>
+                            <li><b>Supportive Ecosystem:</b> By integrating agro-professionals, extension officers, and training resources, MazaoHub ensures farmers have access to the expertise and support they need to succeed.</li>
                         </ul>
-                        Accessible via mobile and web applications, FarmUnity is tailored to rural areas with multilingual support and offline connectivity through SMS and USSD. 
-                        Our vision is to modernize and democratize farming for small-scale producers worldwide.
+                        Accessible via mobile and web applications, MazaoHub is designed to cater to farmers in rural areas by offering multilingual support, mobile-friendly interfaces, and offline connectivity options through SMS and USSD. The platform’s holistic approach fosters collaboration, enhances productivity, and creates a sustainable agricultural ecosystem that uplifts farming communities and contributes to global food security. 
+            With a vision to transform agriculture and a mission to empower farmers, MazaoHub is not just a platform; it’s a movement to modernize and democratize farming for small-scale producers worldwide.
                     </p>
                 </div>
                 <div data-aos='slide-left' data-aos-offset='50' className={`col-md-6 ${styles.imgContain}`}>
